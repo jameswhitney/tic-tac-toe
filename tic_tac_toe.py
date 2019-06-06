7c89e1602be06de202900a8df5e5d578237ba55c
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-06-06 10:37:49 -0400

    Initial Commit

---
+++ tic_tac_toe.py
@@ -0,0 +1,40 @@
+######################################################################
+# Create a simple tic tac toe game that takes user input, X or O.    #
+# Each player takes a turn selecting a position on the board, 1 - 9. #
+# Check for winning combination or if the combination is a draw.     #
+# Regardless of a win or draw, ask the players if they want to       #
+# play again.                                                        #
+# ####################################################################
+
+
+def show_board(board):
+    
+    # TODO: Create a blank tic tac toe board
+    pass
+
+
+def user_input():
+
+    # TODO: Accept user input, check to make sure the input is either an X or O
+    # Assign the input to a marker to represent X or O. The marker will be
+    # used to determine which input will display on the board.
+    pass
+
+
+def place_marker(board, marker, position):
+
+    # TODO: Accept a board list object, either X or O as a marker
+    # and the desired position for the marker on the board. Use
+    # the board list index 1 - 9 to determine where on the board to
+    # place the marker.
+    pass
+
+
+def check_for_win(board, mark):
+
+    # TODO: Accept board list object and mark, X or O, to determine
+    # check what mark has won.
+    pass
+
+
+# Test each function 
